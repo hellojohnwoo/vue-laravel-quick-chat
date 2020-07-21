ed2278eeee077e21bdcdf808cbf5ed207948b098
WOOYOUNG JEONG <hellojohnwoo@gmail.com>
2020-07-21 19:29:58 +0900

    0721 prevent chatting with myself

---
+++ resources/js/components/ChatUserList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="w-1/5 border-r-2 border-solid border-gray-600">
+        <div
+            v-for="user in usersWithoutSignedInUser"
+            :key="user.id"
+            class="p-2 border-b-2 border-gray-600 hover:bg-gray-300"
+        >
+            {{ user.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            currentUser: {
+                type: Number,
+                required: true
+            }
+        },
+
+        computed: {     // prevent chatting with myself
+            usersWithoutSignedInUser() {
+                return this.users.filter(user => user.id !== this.currentUser);
+            }
+        },
+
+        data() {
+            return {
+                users: []
+            }
+        },
+
+        created() {
+            axios.get('/api/users').then(res => {
+                // console.log(res);
+                this.users = res.data.users;
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    }
+</script>
+
+
+
+<!--<style>-->
+
+<!--</style>-->
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex-1">
+        <chat-component :current-user="{{ auth()->id() }}"></chat-component>
+    </div>
+@endsection
